d6f79a2cee385a9249cd627a029ab0126931f2ff
Matt <matt@mspacecreative.com>
2019-03-15 09:55:30 -0300

    c

--- wp-content/themes/stratusx-child/templates/content-performance.php
+++ wp-content/themes/stratusx-child/templates/content-performance.php
@@ -2,31 +2,31 @@
 /* Template Name: Quarterly Performance */
 ?>
 <section class="content-editor">
+<?php
+
+// check if the repeater field has rows of data
+if( have_rows('quarter_ended') ): ?>
+
+<table>
+
+ 	<?php while ( have_rows('quarter_ended') ) : the_row(); ?>
+ 		
+ 		<tr>
+			<td><?php the_field('quarter_end_date'); ?></td>
+			<td><?php the_field('nav_per_share'); ?></td>
+		</tr>
+
+   <?php endwhile; ?>
+   
+</table>
+
+<?php else :
+
+    // no rows found
+
+endif;
+?>
 <?php while (have_posts()) : the_post(); ?>
-	<?php
-	
-	// check if the repeater field has rows of data
-	if( have_rows('quarter_ended') ): ?>
-	
-	<table>
-	
-	 	<?php while ( have_rows('quarter_ended') ) : the_row(); ?>
-	 		
-	 		<tr>
-				<td><?php the_field('quarter_end_date'); ?></td>
-				<td><?php the_field('nav_per_share'); ?></td>
-			</tr>
-	
-	   <?php endwhile; ?>
-	   
-	</table>
-	
-	<?php else :
-	
-	    // no rows found
-	
-	endif;
-	?>
     <?php the_content(); ?>
     <?php if(!is_front_page()){
         wp_link_pages(array('before' => '<nav class="pagination th-pagination">', 'after' => '</nav>'));
