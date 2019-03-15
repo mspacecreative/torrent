35f1247188b437b1837aaa9badacee094b19c06c
Matt <matt@mspacecreative.com>
2019-03-15 10:09:50 -0300

    c

--- wp-content/themes/stratusx-child/templates/content-performance.php
+++ wp-content/themes/stratusx-child/templates/content-performance.php
@@ -1,23 +1,29 @@
 <?php
 /* Template Name: Quarterly Performance */
 ?>
-<section class="content-editor">
+<?php global $post;  ?>
+<?php include( locate_template( 'templates/page-layout.php' ) ); ?>
+<div class="inner-container">
+	<?php include( locate_template( 'templates/page-header.php' ) ); // Page Header Template ?>
 
-<?php if( have_rows('quarter_ended') ): ?>
-	<table>
-	 	<tr>
-	 		<?php while ( have_rows('quarter_ended') ) : the_row(); ?>
-			<td><?php the_field('quarter_end_date'); ?></td>
-			<td><?php the_field('nav_per_share'); ?></td>
-			<?php endwhile; ?>
-		</tr>
-	</table>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-    <?php the_content(); ?>
-    <?php if(!is_front_page()){
-        wp_link_pages(array('before' => '<nav class="pagination th-pagination">', 'after' => '</nav>'));
-    }?>
-<?php endwhile; ?>
-</section>
+	<section class="content-editor">
+	
+	<?php if( have_rows('quarter_ended') ): ?>
+		<table>
+		 	<tr>
+		 		<?php while ( have_rows('quarter_ended') ) : the_row(); ?>
+				<td><?php the_field('quarter_end_date'); ?></td>
+				<td><?php the_field('nav_per_share'); ?></td>
+				<?php endwhile; ?>
+			</tr>
+		</table>
+	<?php endif; ?>
+	
+	<?php while (have_posts()) : the_post(); ?>
+	    <?php the_content(); ?>
+	    <?php if(!is_front_page()){
+	        wp_link_pages(array('before' => '<nav class="pagination th-pagination">', 'after' => '</nav>'));
+	    }?>
+	<?php endwhile; ?>
+	</section>
+</div>
