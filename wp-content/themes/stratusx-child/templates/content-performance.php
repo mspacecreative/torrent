1039da423914b2b23181b564aede5ba3321002f1
Matt <matt@mspacecreative.com>
2019-03-15 10:26:34 -0300

    c

--- wp-content/themes/stratusx-child/templates/content-performance.php
+++ wp-content/themes/stratusx-child/templates/content-performance.php
@@ -4,7 +4,7 @@
 <?php global $post;  ?>
 <?php include( locate_template( 'templates/page-layout.php' ) ); ?>
 <div class="inner-container">
-	<?php include( locate_template( 'templates/page-header.php' ) ); // Page Header Template ?>
+	<?php include( locate_template( 'templates/page-header-performance.php' ) ); // Page Header Template ?>
 	<?php if ( get_field('current_quarter' ) ): 
 		the_field('current_quarter');
 	endif; ?>
