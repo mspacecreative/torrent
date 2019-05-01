d09b77eacbe3ddd4c667be3e0d8f4064257bd5bf
Matt <matt@mspacecreative.com>
2019-05-01 11:32:45 -0300

    c

--- wp-content/themes/stratusx-child/js/scripts.js
+++ wp-content/themes/stratusx-child/js/scripts.js
@@ -1,42 +1,56 @@
-$('.post-pagination li').each(function() {
-    var $this = $(this);
-    if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-        $this.remove();
-});
+(function ($, root, undefined) {
+	
+	$(function () {
+		
+		'use strict';
 
-//$('.post-pagination li:only-child').css('float', 'right');
-var $link = $('.post-pagination li:only-child');
-if ($link.hasClass('prev')) {
-	$link.css('float', 'left');
-} else if ($link.hasClass('next')) {
-	$link.css('float', 'right');
-}
-
-function wrapHeight() {
-	var $body = $('body');
-	if ($body.hasClass('single')) {
-		$body.addClass('wrapheight');
-	} else if ($body.hasClass('blog')) {
-		$body.addClass('wrapheight');
-	}else if ($body.hasClass('page-template-content-performance')) {
-		$body.addClass('wrapheight');
-	}
-}
-
-function addWrapHeight() {
-	var $header = $('.banner.headhesive--stick');
-	var $wrap = $('.wrapheight .wrap');
-	$wrap.css('padding-top', $wrap.siblings($header).outerHeight());
-}
-
-$(document).ready(function() {
-	wrapHeight();
-});
-
-$(window).load(function() {
-	addWrapHeight();
-});
-
-$(window).resize(function() {
-	addWrapHeight();
-});
+		$('.subscribe_tab').click(function() {
+			$(this).parent().toggleClass('reveal');
+		});
+		
+		$('.post-pagination li').each(function() {
+		    var $this = $(this);
+		    if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
+		        $this.remove();
+		});
+		
+		//$('.post-pagination li:only-child').css('float', 'right');
+		var $link = $('.post-pagination li:only-child');
+		if ($link.hasClass('prev')) {
+			$link.css('float', 'left');
+		} else if ($link.hasClass('next')) {
+			$link.css('float', 'right');
+		}
+		
+		function wrapHeight() {
+			var $body = $('body');
+			if ($body.hasClass('single')) {
+				$body.addClass('wrapheight');
+			} else if ($body.hasClass('blog')) {
+				$body.addClass('wrapheight');
+			}else if ($body.hasClass('page-template-content-performance')) {
+				$body.addClass('wrapheight');
+			}
+		}
+		
+		function addWrapHeight() {
+			var $header = $('.banner.headhesive--stick');
+			var $wrap = $('.wrapheight .wrap');
+			$wrap.css('padding-top', $wrap.siblings($header).outerHeight());
+		}
+		
+		$(document).ready(function() {
+			wrapHeight();
+		});
+		
+		$(window).load(function() {
+			addWrapHeight();
+		});
+		
+		$(window).resize(function() {
+			addWrapHeight();
+		});
+	
+	});
+		
+})(jQuery, this);
--- wp-content/themes/stratusx-child/templates/content.php
+++ wp-content/themes/stratusx-child/templates/content.php
@@ -1,4 +1,5 @@
 <div class="subscribe_button">
+	<div class="subscribe_tab"></div>
 	<?php if ( is_active_sidebar( 'subscribe-sidebar' ) ) :
 		dynamic_sidebar( 'subscribe-sidebar' );
 	endif; ?>
